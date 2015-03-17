23b7617f582b8277c96bb9c3ad67c0986cac169e
Sam Boling <charles.samuel.boling@gmail.com>
2015-03-17 16:55:06 -0400

    preamble working

--- analysis.py
+++ analysis.py
@@ -3,10 +3,12 @@ import os
 
 import ocean
 
-def main(fname, path=os.path.abspath("."), simpath=None):
+def main(script_name, schematic,
+         path=os.path.abspath("."), simpath=None):
   if simpath == None:
-    simpath = path + '/simulation'
-  with ocean.Ocean(fname=fname, simpath=simpath) as oc:
+    simpath = os.path.join(path, 'simulation')
+  with ocean.Ocean(fname=script_name, schematic=schematic, 
+                   path=path, simpath=simpath) as oc:
     pass
 
 if __name__ == '__main__':
@@ -16,12 +18,19 @@ if __name__ == '__main__':
              Automated parametric analysis for some simulations with Cadence Virtuoso.
              '''
            )
-  parser.add_argument('fname', dest=fname, type=file, default=None,
+  parser.add_argument('schematic', type=str,
+                      help='Name of schematic cellview')
+  parser.add_argument('script_name', type=file, 
+                      nargs='?', default=None,
                       help='Name of Ocean script to generate')
-  parser.add_argument('path', dest=path, type=str, default='./virtuoso',
-                      help='Path to virtuoso files, if not ./virtuoso')
-  parser.add_argument('--simpath', dest=simpath, type=str, default=None,
-                      help='Directory in which to run the simulator, if not ./simulation')
+  parser.add_argument('--path', dest='path', type=str, 
+                      nargs='?', default='../virtuoso',
+                      help='Path to virtuoso files, if not ../virtuoso')
+  parser.add_argument('--simpath', dest='simpath', 
+                      type=str, default=None,
+                      help='Directory in which to run the simulator, if not <path>/simulation')
   args = parser.parse_args()
 
-  main(args.fname, path=args.path, simpath=args.simpath)
+  main(schematic=args.schematic, 
+       script_name=args.script_name, 
+       path=args.path, simpath=args.simpath)
--- ocean.py
+++ ocean.py
@@ -4,14 +4,22 @@ import os
 import sys
 from functools import partial
 
-class Ocean(object):
-  def __init__(self, schematic, fname=None, simpath=os.path.abspath(".") + "/simulation"):
+class OceanManager(object):
+  def __init__(self, schematic, path, simpath):
     self.schematic = schematic
-    self.fname = fname
     self.path = path
     self.simulator = "spectre"
+    self.abs_simpath = os.path.join(simpath, 
+                                    self.schematic, 
+                                    self.simulator)
 
-    self.virtuoso_simpath = '/'.join([simpath, self.schematic, self.simulator])
+class Ocean(object):
+  def __init__(self, fname, *args, **kwargs):
+    self.fname = fname
+    self.mgr = OceanManager(*args, **kwargs)
+
+  def __exit__(self, type, value, traceback):
+    self.f.close()
 
   def __enter__(self):
     if self.fname == None:
@@ -19,21 +27,41 @@ class Ocean(object):
     else:
       self.f = open(self.fname, 'w')
 
-    self.simulator("'" + self.simulator)
-    self.design(self.virtuoso_simpath + "/netlist/netlist")
-    self.resultsDir(self.virtuoso_simpath + "/schematic")
-    self.path('/'.join([os.environ['CDK_DIR'], "models", self.simulator, "nom"])
+    self.simulator(self._quote(self.mgr.simulator))
+    self.design(
+      self._path(
+        self.mgr.abs_simpath, 
+        "netlist/netlist"))
+    self.resultsDir(
+      self._path(
+        self.mgr.abs_simpath, 
+        "schematic"))
+    self.path(
+      self._path(
+        os.environ['CDK_DIR'], 
+        "models", 
+        self.mgr.simulator, 
+        "nom"))
 
     return self
 
-  def __exit__(self, type, value, traceback):
-    self.f.close()
+  @staticmethod
+  def _quote(x):
+    return "'" + x
 
-  def put(self, *args):
-    self.f.write('(' + '\n  '.join(args) + ')\n')
+  @staticmethod
+  def _string(x):
+    return repr(x).replace("'", "\"")
+
+  @classmethod
+  def _path(cls, *args):
+    return cls._string(os.path.abspath(os.path.join(*args)))
+
+  def _put(self, *args):
+    self.f.write('(' + '\n '.join(args) + ')\n')
 
   def __getattr__(self, name):
-    return partial(self.put, name)
+    return partial(self._put, name)
 
 if __name__ == '__main__':
   with Ocean() as o:
