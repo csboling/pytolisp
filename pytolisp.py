3be3a738f966036005ddd2a20b037e6706e5817f
Sam Boling <charles.samuel.boling@gmail.com>
2015-03-18 23:58:09 -0400

    uses a hack to get around scope issues

--- pytolisp.py
+++ pytolisp.py
@@ -4,6 +4,7 @@ A shallow embedding to compile a subset of Python to Lisp.
 
 import argparse
 import sys
+from StringIO import StringIO
 
 import inspect
 import ast
@@ -13,9 +14,16 @@ import lispify as lpfy
 def compose(*args):
   return reduce(lambda f, g: lambda x: f(g(x)), args)
 
-class ToLisp(ast.NodeVisitor):
-  def __init__(self, outf, *args, **kwargs):
-    ast.NodeVisitor.__init__(self, *args, **kwargs)
+class Embedding(object):
+  '''
+  This only operates on ast.Expr, so assignments, return
+  expressions, etc. should be excluded. Therefore you
+  basically write a 'script' by not assigning any names,
+  and
+  '''
+  def __init__(self, outf=None):
+    if outf == None:
+      outf = StringIO()
     self.outf = outf
 
   @staticmethod
@@ -48,20 +56,40 @@ class ToLisp(ast.NodeVisitor):
   def visit_Expr(self, node):
     parsed = self.toList(node.value)
     self.outf.write(lpfy.lispify(parsed) + '\n')
+    return ast.Expr() # replace with an empty node
+
+def parse_ast(t, outf=None):
+  embedding = Embedding(outf)
+  for __x in t:
+    if isinstance(__x, ast.Expr):
+      embedding.visit_Expr(__x)
+    else:
+      exec(meta.dump_python_source(__x))
+
+  try:
+    return embedding.outf.getvalue()
+  except AttributeError:
+    return None
+
+def parse_func(func, outf=None):
+  source = inspect.getsource(func)
+  func_ast = ast.parse(source).body[0]
+  return parse_ast(func_ast.body, outf=outf)
+
+def parse(source, outf=None):
+  return parse_ast(ast.parse(source), outf=outf)
 
 def example():
   y * 2
+  x = 7*'\x30'
+  print x
   [y] * 2
   str('3')
 
-def parse_func(outf, func):
-  t = ast.parse(inspect.getsource(func))
-  ToLisp(outf).visit(t)
-
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('fname', type=argparse.FileType('w'), 
+  parser.add_argument('outfname', type=argparse.FileType('w'), 
                       nargs='?', default=sys.stdout)
   args = parser.parse_args()
 
-  parse_func(args.fname, example)
+  parse_func(example, outf=args.outfname)
